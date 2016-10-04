642615b0c62831ae23a6ec3aa8a7e7eda271a711
zackman0010 <computerboyzack@gmail.com>
2016-10-03 21:36:36 -0500

    Fixed Various Bugs We Both Missed
    And we both facepalmed at in voice chat

--- room.init.js
+++ room.init.js
@@ -1,7 +1,7 @@
 var roomInit = 
 {
 	//NOTE TO SELF IN CASE I FORGET: To fix this bug, split into two inits because items created don't actually exist until the second tick.
-	first: function(spawn)
+	'first': function(spawn)
 	{
 		//Set variable array for all energy sources in room
 		var sources = spawn.room.find(FIND_SOURCES);
@@ -11,7 +11,7 @@ var roomInit =
 		for(var x = 0;x < sources.length;x++)
 		{
 			//For each energy source in room, create a flag and add it to the room memory array
-			var flagname = sources[x].pos.createFlag('SourceFlag' + x.toString())
+			var flagname = sources[x].pos.createFlag('SourceFlag' + x.toString());
 			if (flagname == -3)
 			{
 				//For some unknown reason, the above command is returning -3 (ERR_NAME_EXISTS) on every call. This workaround makes the rest of the code work.
@@ -31,14 +31,14 @@ var roomInit =
 		spawn.room.memory.initialize2 = true;
 	},
 	
-	second: function(spawn)
+	'second': function(spawn)
 	{
 		//Loop through all sources in room to determine harvestable tiles for each
-		for(var sourcename in spawn.room.memory.sourceFlags)
+		for(var sourceindex in spawn.room.memory.sourceFlags)
 		{
-			var source = Game.flags[sourcename];
+			var source = Game.flags[spawn.room.memory.sourceFlags[sourceindex]];
 			//Create and initialize variable for number of tiles around the source that can be harvested from
-			Game.flags[sourcename]source.memory.availHarvest = 0;
+			source.memory.availHarvest = 0;
 			source.memory.actHarvest = 0;
 			
 			//Loop through the tiles within a 1-tile radius of the source.
