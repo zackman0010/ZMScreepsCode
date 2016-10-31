f41b2a5d93ef2776553fd45a3581c4e338e4eca7
zackman0010 <computerboyzack@gmail.com>
2016-10-31 00:23:41 -0500

    Revert "Continued work on RCL2 Spawn dictionary (added in emergency Upgrader, code within main to allow Upgraders to work if the RCL downgrade timer is running low, and other tweaks to code)"
    This reverts commit 8721b28dadaa550e9b325cec446e7b343c81b6a1.

--- spawn.dict.js
+++ spawn.dict.js
@@ -22,26 +22,11 @@
             //No condition
         else if(condition == 'emergency') return (thisRoom.memory.harvesterCt < 3);//NOTE TO SELF: harvesterCt does not exist yet
             //Emergency conditions -- spawn a regular harvester regardless of normal harvester size
-        else if(condition == 'downgrade') return (thisRoom.controller.ticksToDowngrade < 2000);
-            //Emergency condition -- spawn an Upgrader to prevent room controller downgrade
         else if(condition == 'etier1') return (thisRoom.energyCapacityAvailable < 550);
             //Energy tier 1 -- spawner cannot create 550-energy Creeps
         else if(condition == 'etier2') return (thisRoom.energyCapacityAvailable >= 550 && thisRoom.energyCapacityAvailable < 800);
             //Energy tier 2 -- spawner can create 550-energy creeps but not 800-energy creeps
-        else if(condition == 'build')
-        {
-            var damTar = thisRoom.find(FIND_STRUCTURES,
-            {
-                //Detect all damaged structures (or Ramparts and Walls with less than a desired max HP)
-                filter: (structure) =>
-                {
-                    return ((structure.structureType == STRUCTURE_RAMPART && structure.hits < 10000) ||
-                            (structure.structureType == STRUCTURE_WALL && structure.hits < 10000) ||
-                            (structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_RAMPART && structure.structureType != STRUCTURE_WALL))
-                }
-            });
-            return (thisRoom.memory.buildSites > 0 || damTar.length > 0);
-        }
+        else if(condition == 'build') return (thisRoom.memory.buildSites > 0);//NOTE TO SELF: Add in code to detect damaged structures
             //Construction sites or damaged structures are present in room
         else
         {
@@ -78,38 +63,25 @@
             qty: 3,
             condition: 'emergency'
         },
-        '2'://Emergency Upgrader (failsafe to prevent RCL downgrade)
-        {
-            type: 'upgrader',
-            qty: 1,
-            condition: 'downgrade'
-        },
-        '3'://First main priority: Room qty of Harvesters (if RCL2 extensions not finished)
+        '2'://First main priority: Room qty of Harvesters (if RCL2 extensions not finished)
         {
             type: 'harvester',
             qty: 11,//PLACEHOLDER. Replace with code to find room.memory.totalHarvest
             condition: 'etier1'
         },
-        '4'://First main priority: Room qty of Harvester2s (if RCL2 extensions finished)
+        '3'://First main priority: Room qty of Harvester2s (if RCL2 extensions finished)
         {
             type: 'harvester2',
             qty: 11,//PLACEHOLDER. Replace with code to find room.memory.totalHarvest
             condition: 'etier2'
         },
-        '5'://Third main priority: Single Builder (to begin construction work ASAP)
-        {
-            //(Miles note: This is only really a good idea if build code can be modified to start building stuff before spawner deactivates saving)
-            type: 'builder',
-            qty: 1,
-            condiion: 'build'
-        },
-        '6'://Fourth main priority: Handful of Upgraders (Miles note: Argue about exact number later)
+        '4'://Second main priority: Handful of Upgraders (Miles note: Argue about exact number later)
         {
             type: 'upgrader',
             qty: 5,
             condition: 'true'
         },
-        '7'://Fifth main priority: Handful of Builders (if there are construction sites or repairs needed) (Miles note: Argue about exact number later)
+        '5'://Third main priority: Handful of Builders (if there are construction sites or repairs needed) (Miles note: Argue about exact number later)
         {
             type: 'builder',
             qty: 3,
